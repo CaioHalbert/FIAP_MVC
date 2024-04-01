fc281bd5245c16bd67f1a530448357cbdff436ca
caiohalbert <caio.halbert@gmail.com>
2024-04-01 20:53:55 -0300

    Inclusão de model e data Context, Dependencias - entity framework core, tools e oracle.

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using FIAP_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FIAP_MVC.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
